75021cc6a110d23868e6b96820581e62762625f5
Laurent-Daleo <137190234+Laurent-Daleo@users.noreply.github.com>
2024-03-09 18:48:58 +0100

    add: link to logo for user UX

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import svg from './svg/LOGO.svg'
 import './logo.scss'
+import { Link } from 'react-router-dom'
+
 const logo = () => {
   return(
+    <Link to="/">
     <div className="logo">
       <img src={svg} alt="Logo Kasa" />
     </div>
+    </Link>
   )
 }
 export default logo
